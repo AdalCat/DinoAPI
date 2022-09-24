d4fce396560d0792ff1b6dc772acc41ef1455d17
Gonzalo Ramos <gnzramos@gmail.com>
2022-09-24 01:27:41 -0500

    Se agrega conexion con base de datos en Render

--- config/db.js
+++ config/db.js
@@ -1,5 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sqlite://db.sqlite');
+// const sequelize = new Sequelize('sqlite://db.sqlite');
+
+// Conexión a DB externa (Render)
+const sequelize = 
+new Sequelize(
+    'postgres://dinoapi_user:l56YgZxNrfFmKzFvAeZCdLSCkocahk88@dpg-ccmlkb02i3mirn7nvofg-a.ohio-postgres.render.com/dinoapi?ssl=true'
+    )
 
 module.exports = sequelize; 
