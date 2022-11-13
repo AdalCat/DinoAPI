a3b55587b240bce80f37e4efe04aa8d5015b3dfc
Angel <angelSistemas21@gmail.com>
2022-11-13 15:46:08 -0600

    feat: Added dialectOptions field for ssl

--- config/config.js
+++ config/config.js
@@ -6,7 +6,13 @@ const defaultConfig = {
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    "dialectOptions": {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    }
 }
 
 module.exports = {
